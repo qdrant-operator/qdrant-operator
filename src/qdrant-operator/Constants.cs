49c7b5e12dedcd850fe1624aa495f18859fe0b15
Carolina Haggerty <carolina.vhaggerty@gmail.com>
2024-02-24 13:07:19 -0800

    initial OLM

--- src/qdrant-operator/Constants.cs
+++ src/qdrant-operator/Constants.cs
@@ -11,6 +11,16 @@ namespace QdrantOperator
         public const string QdrantStorage = "qdrant-storage";
         public const string QdrantStoragePath = "/qdrant/storage";
         public const string OwnershipInitContainerName = "ensure-storage-dir-ownership";
+        public const string ShortDescription = "The Qdrant Operator for Kubernetes is an operator for managing Qdrant Clusters in " +
+            "a Kubernetes Cluster.";
+        public const string FullDescription = $"The Qdrant Kubernetes Operator automates the deployment and management of Qdrant " +
+            $"clusters on Kubernetes. The operator allows you to deploy clusters, create collections, define vectors and manage indexes.";
+        public const string QdrantClusterDescription = $"QdrantCluster resources describe a Qdrant cluster";
+        public const string QdrantCollectionDescription = $"QdrantCollection resources describe a collection. " +
+            $"Each cluster can have many collections.";
+        public const string QdrantCollectionFieldIndexDescription = $"A QdrantFieldIndex describes a field index for a collection. " +
+            $"Each collection can have many field indexes.";
+
 
         public static string HeadlessServiceName(string resourceName)
         {
---
+++ tests/Test.qdrant-operator/OlmAttributes.cs
@@ -0,0 +1,26 @@
+using Neon.Operator.Attributes;
+using Neon.Operator.OperatorLifecycleManager;
+
+using QdrantOperator;
+
+[assembly: Name(Name = "qdrant-operator")]
+[assembly: DisplayName(DisplayName = "Qdrant Operator")]
+[assembly: OwnedEntity<V1QdrantCluster>(Description = Constants.QdrantClusterDescription, DisplayName = "QdrantCluster")]
+[assembly: OwnedEntity<V1QdrantCollection>(Description = Constants.QdrantCollectionDescription, DisplayName = "QdrantCollection")]
+[assembly: OwnedEntity<V1QdrantCollectionFieldIndex>(Description = Constants.QdrantCollectionFieldIndexDescription, DisplayName = "QdrantCollectionFieldIndex")]
+[assembly: Description(FullDescription = Constants.FullDescription, ShortDescription = Constants.ShortDescription)]
+[assembly: Provider(Name = "qdrant-operator", Url = "https://github.com/qdrant-operator")]
+[assembly: Maintainer(Name = "Marcus", Email = "marcus@bowyer.me")]
+[assembly: Maintainer(Name = "Carolina", Email = "carolina.vhaggerty@gmail.com")]
+[assembly: Maintainer(Name = "123455", Email = "carolina.vhaggerty@gmail.com")]
+[assembly: Version("0.0.1")]
+[assembly: Maturity("beta")]
+[assembly: MinKubeVersion("")]
+[assembly: Icon(Path = "../../logo.png", MediaType = "image/png")]
+[assembly: Keyword("qdrant")]
+[assembly: Category(Category = Category.AiMachineLearning | Category.BigData | Category.Database)]
+[assembly: Capabilities(Capability = CapabilityLevel.FullLifecycle)]
+[assembly: ContainerImage(Repository = "ghcr.io/qdrant-operator/qdrant-operator", Tag = "0.0.1")]
+[assembly: Repository(Repository = "https://github.com/qdrant-operator/qdrant-operator")]
+[assembly: InstallMode(Type = InstallModeType.OwnNamespace | InstallModeType.SingleNamespace | InstallModeType.MultiNamespace | InstallModeType.AllNamespaces)]
+[assembly: DefaultChannel("stable")]
